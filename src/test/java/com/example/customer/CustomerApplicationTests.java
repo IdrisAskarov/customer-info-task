389ba0f8a40a08ca77368a7852b495baec813945
Idris Askarov <askid.me@gmail.com>
2022-06-14 22:25:56 -0500

    updated test suits

--- src/test/java/com/example/customer/CustomerApplicationTests.java
+++ src/test/java/com/example/customer/CustomerApplicationTests.java
@@ -14,6 +14,7 @@ class CustomerApplicationTests {
     @Test
     void testOctogenarian(){
         OctogenarianTest octogenarianTest = new OctogenarianTest();
+
         Assert.assertFalse(octogenarianTest.isOctogenarian(79));
         Assert.assertTrue(octogenarianTest.isOctogenarian(80));
         Assert.assertTrue(octogenarianTest.isOctogenarian(81));
