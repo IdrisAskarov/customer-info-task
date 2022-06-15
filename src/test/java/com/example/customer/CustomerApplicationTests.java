355c6204904c0849dde8d07aa78e555245748466
Idris Askarov <askid.me@gmail.com>
2022-06-14 21:20:12 -0500

    updated test suits

--- src/test/java/com/example/customer/CustomerApplicationTests.java
+++ src/test/java/com/example/customer/CustomerApplicationTests.java
@@ -1,8 +1,11 @@
 package com.example.customer;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.Assert;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 
 @SpringBootTest
 class CustomerApplicationTests {
--- src/test/java/com/example/customer/request/get/GetCustomer.java
+++ src/test/java/com/example/customer/request/get/GetCustomer.java
@@ -2,11 +2,15 @@ package com.example.customer.request.get;
 
 
 import com.example.customer.base_urls.CustomerBaseUrl;
+import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.example.customer.model.Customer;
+
+import java.time.ZoneId;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
@@ -16,19 +20,41 @@ public class GetCustomer extends CustomerBaseUrl {
 
     @Test
     public void getCustomerByEmail() {
-        spec.pathParam("email", "ernie.hudson@hotmail.com");
+        Faker faker = new Faker();
+        String email = faker.internet().emailAddress();
+        Customer customer = new Customer();
+        customer.setFirstName(faker.name().firstName());
+        customer.setLastName(faker.name().lastName());
+        customer.setEmailAddress(email);
+        customer.setPassword(faker.internet().password());
+        customer.setBirthDate(faker.date().birthday().toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate());
 
         Response response = given()
+                .spec(spec)
+                .contentType(ContentType.JSON)
+                .body(customer)
+                .when()
+                .post("/add");
+
+        response.then().statusCode(200);
+        response.prettyPrint();
+
+        spec.pathParam("email", email);
+
+
+        response = given()
                 .spec(spec)
                 .accept(ContentType.JSON)
                 .when()
                 .get("/get/{email}");
-        String prettyPrint2 = response.prettyPrint();
+        response.prettyPrint();
 
         response
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("emailAddress",equalTo("ernie.hudson@hotmail.com"));
+                .body("emailAddress",equalTo(email));
     }
 }
