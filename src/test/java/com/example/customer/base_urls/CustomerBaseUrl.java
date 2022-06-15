00b36f0d97de2be542d8e3ff977da14a83e07c86
Idris Askarov <askid.me@gmail.com>
2022-06-14 22:31:52 -0500

    updated test suits

--- src/test/java/com/example/customer/base_urls/CustomerBaseUrl.java
+++ src/test/java/com/example/customer/base_urls/CustomerBaseUrl.java
@@ -4,9 +4,22 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
+import com.example.customer.model.Customer;
+
+import java.time.ZoneId;
 
 public class CustomerBaseUrl {
-
+    public static Customer customer;
+    /*static {
+        Customer customer = new Customer();
+        customer.setFirstName(faker.name().firstName());
+        customer.setLastName(faker.name().lastName());
+        customer.setEmailAddress(faker.internet().emailAddress());
+        customer.setPassword(faker.internet().password());
+        customer.setBirthDate(faker.date().birthday().toInstant()
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate());
+    }*/
     protected RequestSpecification spec;
 
     @BeforeEach
